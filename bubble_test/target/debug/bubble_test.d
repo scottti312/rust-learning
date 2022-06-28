b197954b48c0a50ccb60273d384eff4c6453e67c
Scott <scott.ti312@gmail.com>
2022-06-28 10:10:24 -0700

    rm bubble_test bin

--- bubble_test/target/debug/bubble_test.d
+++
@@ -1,1 +0,0 @@
-/home/scott/Desktop/rust/bubble_test/target/debug/bubble_test: /home/scott/Desktop/rust/bubble_test/src/main.rs
--- bubble_test/target/debug/deps/bubble_test-fa7584bd7ac53f83.d
+++
@@ -1,5 +0,0 @@
-/home/scott/Desktop/rust/bubble_test/target/debug/deps/bubble_test-fa7584bd7ac53f83.rmeta: src/main.rs
-
-/home/scott/Desktop/rust/bubble_test/target/debug/deps/bubble_test-fa7584bd7ac53f83.d: src/main.rs
-
-src/main.rs:
