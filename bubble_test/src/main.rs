498436307395ac5b6226ab307f388c9b67cc5c08
Scott <scott.ti312@gmail.com>
2022-06-28 01:12:28 -0700

    generic type and indefinite array size

--- bubble_test/src/main.rs
+++ bubble_test/src/main.rs
@@ -1,21 +1,15 @@
 fn main() {
-    let mut a1 = [5, 6, 2, 2, -6, 23, 3, 1];
+    let mut a1 = ['a', 'g', 'e', 'k', 'z', 'x'];
     println!("Start = {:?}", a1);
-    a1 = bubble_sort(a1);
+    bubble_sort(&mut a1);
     println!("Result = {:?}", a1);
 }
-fn swap(mut first: i32, mut second: i32) -> (i32, i32) {
-    let temp = first;
-    first = second;
-    second = temp;
-    (first, second)
-}
 
-fn bubble_sort(mut a1: [i32; 8]) -> [i32; 8] {
+fn bubble_sort<T: Ord>(a1: &mut [T]) -> &mut [T] {
     for _i in 0..a1.len() - 1 {
         for j in 0..a1.len() - 1 {
             if a1[j + 1] < a1[j] {
-                (a1[j], a1[j + 1]) = swap(a1[j], a1[j + 1]);
+                a1.swap(j + 1, j);
             }
         }
     }
@@ -28,16 +22,16 @@ mod tests {
 
     #[test]
     fn descending_test() {
-        let inital_array = [8, 7, 6, 5, 4, 3, 2, 1];
-        let actual = bubble_sort(inital_array);
+        let mut inital_array = [8, 7, 6, 5, 4, 3, 2, 1];
+        let actual = bubble_sort(&mut inital_array);
         let expected = [1, 2, 3, 4, 5, 6, 7, 8];
         assert_eq!(expected, actual);
     }
 
     #[test]
     fn random_test() {
-        let initial_array = [5, 6, 2, 2, -6, 23, 3, 1];
-        let actual = bubble_sort(initial_array);
+        let mut initial_array = [5, 6, 2, 2, -6, 23, 3, 1];
+        let actual = bubble_sort(&mut initial_array);
         let expected = [-6, 1, 2, 2, 3, 5, 6, 23];
         assert_eq!(actual, expected);
     }
