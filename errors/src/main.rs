267fdebe7e7c626b444dd1bc723f913121223cac
Scott <scott.ti312@gmail.com>
2022-07-01 03:00:06 -0700

    add ? operator shortcut

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -16,7 +16,17 @@ fn read_username_from_file() -> Result<String, io::Error> {
     }
 }
 
+fn short_read_username_from_file() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
 fn main() {
     let result = read_username_from_file();
     println!("{:?}", result);
+
+    let short_result = short_read_username_from_file();
+    println!("{}", short_result.unwrap());
 }
